4ae2e4194c1bf42c3f26cb856afe5fedffa68fcc
rbblazquez <rubenbarroso16@gmail.com>
2023-12-30 01:04:44 +0100

    refactor: rename sport_monks dag_builder file for download_dag_builder

--- data_pipelines/dags/sport_monks_dags.py
+++ data_pipelines/dags/sport_monks_dags.py
@@ -1,6 +1,6 @@
 # *-* airflow DAG script *-*
 
-from data_pipelines.sport_monks.dag_builder import build_sport_monks_dags
+from data_pipelines.sport_monks.download_dag_builder import build_sport_monks_dags
 
 for dag in build_sport_monks_dags():
     globals()[dag.dag_id] = dag
