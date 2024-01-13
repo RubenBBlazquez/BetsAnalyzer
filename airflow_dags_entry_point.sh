1e484d380b7acfe6520bb75ce184b80491b975d0
rbblazquez <rubenbarroso16@gmail.com>
2024-01-13 22:34:52 +0100

    fix: remove data_pipelines egg-info

--- airflow_dags_entry_point.sh
+++ airflow_dags_entry_point.sh
@@ -7,6 +7,6 @@ rm -f /opt/airflow/dags/sport_monks_etl_dags.py
 ln -sf "/opt/airflow/data_pipelines/dags/sport_monks_etl_dags.py" "/opt/airflow/dags/sport_monks_etl_dags.py"
 
 pip install -e /opt/airflow/data_pipelines
-rm -f /opt/airflow/data_pipelines/data_pipelines.egg-info
+rm -rf /opt/airflow/data_pipelines/data_pipelines.egg-info
 
 exec airflow "$@"
