3696da614c2baee987df996f4c3c9eb7910fb902
rbblazquez <rubenbarroso16@gmail.com>
2024-04-02 22:06:18 +0200

    [refactor]: remove builder and collector from downloaders, it is not necessary

--- data_pipelines/downloaders/downloader_dags_builder.py
+++ data_pipelines/downloaders/downloader_dags_builder.py
@@ -1,57 +1,42 @@
 from datetime import datetime
+from typing import Iterable
 
-import attr
 from airflow.models.dag import DAG
 from common.downloader_base import Downloader
 from downloaders.sport_monks.builder import get_sport_monks_downloaders
 
-from data_pipelines.common.dag_builder import DagCollector, IDagBuilder
 
-
-@attr.s(auto_attribs=True)
-class DownloaderDagBuilder(IDagBuilder):
+def build_downloader_dag(downloader: Downloader) -> DAG:
     """
-    Class for building DAG for downloading data from SportMonks API
-
-    Attributes
-    -----------
-    _downloader: Downloader
-        entity  that contains all the information necessary
-        to download data
-
+    Method to build a DAG for a downloader
     """
+    today = datetime.today()
+    entity = downloader.entity
 
-    _downloader: Downloader
+    dag = DAG(
+        dag_id=f"Downloader_{entity.dag_name}",
+        schedule=entity.schedule,
+        start_date=datetime(today.year, today.month, today.day),
+    )
 
-    def build(self):
-        today = datetime.today()
-        entity = self._downloader.entity
-        dag = DAG(
-            dag_id=f"Downloader_{entity.dag_name}",
-            schedule=entity.schedule,
-            start_date=datetime(today.year, today.month, today.day),
-        )
+    for task in downloader.get_downloader_tasks():
+        dag.add_task(task)
 
-        for task in self._downloader.get_downloader_tasks():
-            dag.add_task(task)
-
-        return dag
+    return dag
 
 
-def build_sports_monks_dags(dag_collector: DagCollector):
+def build_sports_monks_dags() -> Iterable[DAG]:
     """
     Method to build sport monks DAGs
     """
     for sport_monks_downloader in get_sport_monks_downloaders():
-        dag_collector.add_builder(DownloaderDagBuilder(sport_monks_downloader))
+        yield build_downloader_dag(sport_monks_downloader)
 
 
 def build_downloader_dags() -> list[DAG]:
     """
     Method to build DAGs for downloading data from SportMonks API
     """
-    dag_collector = DagCollector()
+    sport_monks_dags = build_sports_monks_dags()
 
-    build_sports_monks_dags(dag_collector)
-
-    return dag_collector.collect()
+    return [*sport_monks_dags]
